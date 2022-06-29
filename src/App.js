1d46934e2b8ff274240a96eaaa2c1a74e92fac51
LeKhanhGithub <khanhcong1909@gmail.com>
2022-06-29 17:02:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* use flex */}
+    <div className="box">
+        <div className="box-big1">box bog 1</div>
+        <div className="box-big2">
+          <div className="big2-child1"> big2 child</div>
+          <div className="big2-child2">
+            <div className = "child1">child1.1</div>
+            <div className = 'child2'>
+              <div className = "lastchild1" >1</div>
+              <div className = "lastchild2">2</div>
+            
+            </div>
+          </div>
+        </div>
     </div>
+
+{/* use grid */}
+
+    <div className = "box2">
+      <div className = "hoa1">1</div>
+      <div className = "hoa2">2</div>
+      <div className = "hoa3">3</div>
+      <div className = "hoa4">4</div>
+      <div className = "hoa5">5</div>
+    </div>
+    </>
   );
 }
 
